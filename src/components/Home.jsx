64df069a1063d9efc1be53e40f0a77a4c38698cd
Christine Frank and Hailey Gaylor <christine@>
2019-11-14 11:52:22 -0800

    Add Ross signature to home page and some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Bobross from '../assest/images/Bobross.jpg';
-
+import rossSignature from '../assest/images/rossSignature.png';
+import backgroundImage from '../assest/images/Background.jpg';
 
 function Home(){
+    
+    var homeStyling ={
+        textAlign:"center",
+        fontFamily:"lucida Console",
+        backgroundImage:`url(${backgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        color: 'white',
+        fontWeight: 'bold'
+    };
+    var imageStyle = {
+        boxShadow: '0 0 20px 20px inset'
+    };
   return(
-    <div>
+    <div style= {homeStyling}>
       <h1> Happy Cloud Art</h1>
-      <img src= {Bobross}/>
+      <img style={imageStyle} src= {Bobross}/>
       <h4>with</h4>
-      <h2>Bob Ross </h2>
+      <img src= {rossSignature}/>
     </div>
   );
 }
